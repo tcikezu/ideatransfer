fd87fae00fdfed2adc004474c0c642cbb8ee971d
tcikezu <clark@clark-e5570.localdomain>
2019-11-03 13:18:32 -0800

    gifs/ now has better simulations, simulate is now  a function one can call

--- simulate.py
+++ simulate.py
@@ -1,60 +1,53 @@
 """ simulate.py
 -----------------------
 This file creates a community object, defined in test.py, and runs to see how ideas
-evolve over some number of iterations."""
+evolve over some number of iterations. Also creates a gif to see an animation of the ideas."""
 
 import transfer
 from community import *
 import  matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
+import numpy as np
+from datetime import datetime
 
+# There is an option to set a file-name -- would highly recommend doing that.
+date_time = datetime.now().strftime("%m_%d_%Y_%H:%M:%S")
 
-def simulate(X=community(300,3), gamma = 0.005, T = 80):
-    #gamma = 0.005
-    #T = 80
-    #X = community(300,3)
-    x,y,z = [], [], []
+""" simulate returns an animated 3d scatter plot of the community allIdeas object.
+The input parameters are the community, the interaction coefficient gamma, 
+and also fn, a string file-name which defaults to date and time."""
+def simulate(X=community(300,3), gamma = 0.005, T = 80,fn=date_time):
+    fps = 40
+    
+    # Data to store X.allIdeas, to then make animation
+    dataX = np.ndarray((T*fps,X.numberMembers))
+    dataY = np.ndarray((T*fps,X.numberMembers))
+    dataZ = np.ndarray((T*fps,X.numberMembers))
 
-    #fn = 'probabilistic merging'
-    fn = 'deterministic merging'
+    # Iterate the idea transfer throughout community
     for t in range(T):
+        dataX[t,:] = X.allIdeas[:,0]
+        dataY[t,:] = X.allIdeas[:,1]
+        dataZ[t,:] = X.allIdeas[:,2]
         transfer.deterministicMerge(X, gamma)
-        #transfer.probabilisticMerge(X,gamma)
-        x.append(X.allIdeas[:,0])
-        y.append(X.allIdeas[:,1])
-        z.append(X.allIdeas[:,2])
+        #transfer.probabilisticMerge(X, gamma)
 
-
-    fps = 40 
-
+    # Plot results
     fig = plt.figure()
     ax = fig.add_subplot(111,projection='3d')
-    #sct, = ax.plot3D([],[],[],"o",markersize=2)
-    #sct = ax.scatter3D([],[],[])
 
-    def update(ifrm, xa, ya, za):
-        #sct.set_offsets(np.c_[xa[ifrm],ya[ifrm]])
-        #sct.set_3d_properties(za[ifrm])
-        #sct.set_data(xa[ifrm], ya[ifrm])
-        #sct.set_3d_properties(za[ifrm])
+    #animation function for animation.FuncAnimation
+    def update(ifrm,dataX,dataY,dataZ):
         ax.clear()
         plt.autoscale(False)
         ax.set_xticks([-1.0,-0.5,0.0,0.5,1])
         ax.set_yticks([-1.0,-0.5,0.0,0.5,1])
         ax.set_zticks([-1.0,-0.5,0.0,0.5,1])
-        ax.scatter3D(xa[ifrm],ya[ifrm],za[ifrm])
+        ax.scatter3D(dataX[ifrm], dataY[ifrm], dataZ[ifrm])
         ax.set_xlabel("frame: %d" % (ifrm))
          
-    ax.set_xlim(-1,1)
-    ax.set_ylim(-1,1)
-    ax.set_zlim(-1,1)
-
-    ani = animation.FuncAnimation(fig, update, T, fargs=(x,y,z),interval = T/fps )
-
-
-    #ani.save(fn+'.mp4',writer='ffmpeg',fps=fps)
+    ani = animation.FuncAnimation(fig, update, T, fargs=(dataX,dataY,dataZ),interval = T/fps )
     ani.save(fn+'.gif',writer='imagemagick',fps=fps)
 
     plt.show()
-
